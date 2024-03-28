f37ab7aa85749a63bdf2baf001e0e21938345a49
anjanan <athiruma@uwaterloo.ca>
2024-03-28 04:20:34 -0400

    cleaned up

--- FridgePal_custom_model.py
+++ FridgePal_custom_model.py
@@ -2,76 +2,68 @@ import tensorflow as tf
 from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
-import pandas as pd
 
-train_root = r'C:\Users\anjan\OneDrive\Documents\makeUoftDataset\train'
-val_root = r'C:\Users\anjan\OneDrive\Documents\makeUoftDataset\validate'
-test_root = r'C:\Users\anjan\Downloads\archive (4)\fruits-360-original-size\fruits-360-original-size\Test'
+train_root = r'C:\Users\anjan\OneDrive\Documents\fridgePalDataset\train'
+val_root = r'C:\Users\anjan\OneDrive\Documents\fridgePalDataset\validate'
+test_root = r'C:\Users\anjan\OneDrive\Documents\fridgePalDataset\test'
 
-train_data = pd.read_csv(train_root)
-val_data = pd.read_csv(val_annotations)
-test_data = pd.read_csv(test_annotations)
-
-train_grouped_annotations = train_data.groupby('filename')['class'].apply(list).reset_index()
-val_grouped_annotations = val_data.groupby('filename')['class'].apply(list).reset_index()
-test_grouped_annotations = test_data.groupby('filename')['class'].apply(list).reset_index()
-
-datagen = ImageDataGenerator(rescale=1./255)
-
-train_datagen = datagen.flow_from_dataframe(
-    dataframe=train_grouped_annotations
-    x_col='filename',
-    y_col='class'
+train_datagen = ImageDataGenerator(
+   # rescale = 1./255,
+    rotation_range =20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    #shear_range=0.2,
+    #zoom_range=0.2,
+    #horizontal_flip=True,
     #brightness_range=[0.5,1.5],
     #channel_shift_range=30.0,
     fill_mode='nearest')
 
-val_datagen = ImageDataGenerator(rescale = 1./255)
-test_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator()
+test_datagen = ImageDataGenerator()
 
-train_generator = train_datagen.flow_from_dataframe(
+train_generator = train_datagen.flow_from_directory(
     train_root, 
-    target_size=(100,100),
+    target_size=(128,128),
     class_mode='categorical',
-    batch_size=126
+    batch_size=32
 )
 val_generator = val_datagen.flow_from_directory(
     val_root,
-    target_size=(100,100),
+    target_size=(128,128),
     class_mode='categorical',
-    batch_size=126
+    batch_size=32
 )
 test_generator=test_datagen.flow_from_directory(
     test_root,
-    target_size=(100,100),
+    target_size=(128,128),
     class_mode='categorical',
-    batch_size=126
+    batch_size=32
 )
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(filters=32, kernelsize= (5,5), activation='relu', input_shape=(100,100,3)),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(128,128,3)),
     tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Conv2D(filters=64, kernelsize=(5,5), activation='relu'),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
     tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Conv2D(filters=128, kernelsize=(5,5), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
+
 
     tf.keras.layers.Flatten(),
-    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dropout(0.2),
 
     tf.keras.layers.Dense(512, activation='relu'),
-    tf.keras.layers.Dense(24, activation='softmax')
+    tf.keras.layers.Dense(4, activation='softmax')
 ])
 
 model.summary()
 
 model.compile(loss = 'categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
-history = model.fit(train_generator, epochs=25, steps_per_epoch=20, validation_data=val_generator, verbose=1, validation_steps=3)
+history = model.fit(train_generator, epochs=15, steps_per_epoch=10, validation_data=val_generator, verbose=1, validation_steps=3)
 
-test_loss, test_accuracy = model.evaluate(test_generator)
-print("Test loss:", test_loss)
-print("Test Accuracy:", test_accuracy)
+#test_loss, test_accuracy = model.evaluate(test_generator)
+#print("Test loss:", test_loss)
+#print("Test Accuracy:", test_accuracy)
 
 model.save("testMakeUoft.keras")
 
@@ -84,8 +76,8 @@ epochs = range(len(acc))
 
 plt.plot(epochs, acc, 'r', label='Training Accuracy')
 plt.plot(epochs, val_acc, 'b', label='Validation Accuracy')
-plt.plot(len(epochs), test_accuracy, 'g', label='Test Accuracy')
-plt.title('Training, validation, test accuracy')
+#plt.plot(len(epochs), test_accuracy, 'g', label='Test Accuracy')
+plt.title('Training, validation')
 plt.legend(loc=0)
 plt.figure()
 
